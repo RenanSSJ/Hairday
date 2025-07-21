8c6a9cbb81c86ddfc89f174b7fd4747ebbb37755
Renan <renansantos.trab@gmail.com>
2025-07-21 19:35:09 +0100

    added time sections JS

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,8 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours"
+
+const hours = document.getElementById("hours")
+
 export function hoursLoad( { date }) {
   const opening = openingHours.map((hour) => {
   //get hour
@@ -13,4 +16,31 @@ export function hoursLoad( { date }) {
       available: isHourPast,  
     }
   })
+  
+  //show time
+  opening.forEach(({ hour, available}) => {
+    const li = document.createElement("li")
+
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+    li.textContent = hour
+
+    if(hour === "9:00") {
+      hourHeaderAdd("Manhã")
+    }else if (hour === "13:00") {
+      hourHeaderAdd("Tarde")
+    }else if (hour === "18:00"){
+      hourHeaderAdd("Noite")
+    }
+
+    hours.append(li)
+  })
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li")
+  header.classList.add("hour-period")
+  header.textContent = title
+  hours.append(header)
 }
